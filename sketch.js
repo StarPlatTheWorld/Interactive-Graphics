4744c27d91af86b6699339ffaf1adf909d60b7e9
Ethan <mcfarlane-e1@ulster.ac.uk>
2024-03-01 19:14:08 +0000

    Added potential motion for Earth to rotate around the moon, can be changed in the future

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var angle = 0.0; var scaler = 700; var speed = 0.02;
+var x0 = 340; var y0 = 1100;
+
 function setup() {
     createCanvas(680, 680);
 }
@@ -11,11 +14,15 @@ function draw(){
 function drawMoon(){
     fill(128);
     noStroke();
-    ellipse(340, 1100, 1200);
+    ellipse(x0, y0, 1200);
 }
 
 function drawEarth(){
+    x = x0 + cos(angle) * scaler;
+    y = y0 + sin(angle) * scaler;
     fill(0, 0, 255);
     noStroke();
-    ellipse(100, 100, 100);
+    rect(x, y, 200, 50);
+    angle += speed;
+    
 }
